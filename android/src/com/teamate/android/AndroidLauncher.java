56503cd93e19e803522a5de86826f652c4ba2502
Michael Kent <michaelkent94@me.com>
2015-02-22 15:11:45 -0500

    A default libGDX project configured for Android and Box2D. You'll need to change the path in Speed/local.properties to the location of your android sdk installation.

---
+++ android/src/com/teamate/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.teamate.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.teamate.Speed;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Speed(), config);
+	}
+}
